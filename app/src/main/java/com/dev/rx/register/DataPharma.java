aad720eb39c4c77644968e7149dcb9be4435778e
Cristofer Jaimez <jaimez07788@gmail.com>
2023-04-05 19:22:16 -0500

    diseños

---
+++ app/src/main/java/com/dev/rx/register/DataPharma.java
@@ -0,0 +1,16 @@
+package com.dev.rx.register;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.dev.rx.R;
+
+public class DataPharma extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_pharma);
+    }
+}
--- app/src/main/java/com/dev/rx/register/Geo.java
+++ app/src/main/java/com/dev/rx/register/Geo.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.content.res.Resources;
@@ -11,10 +12,13 @@ import android.content.res.XmlResourceParser;
 import android.os.Bundle;
 import android.os.LocaleList;
 import android.util.Xml;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.dev.rx.R;
 import com.dev.rx.R.*;
+import com.dev.rx.login.Login;
 
 import org.xmlpull.v1.XmlPullParserException;
 import org.xmlpull.v1.XmlSerializer;
@@ -28,11 +32,34 @@ import java.util.Locale;
 
 public class Geo extends AppCompatActivity {
 
+    private Button btnBack, btnNext;
+
     @SuppressLint("StringFormatInvalid")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_geo);
+
+        btnBack = findViewById(id.btnBack);
+        btnNext = findViewById(id.btnNext);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Geo.this, Login.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Geo.this, DataPharma.class);
+                startActivity(intent);
+            }
+        });
 
 
 
